866ea6f172e564729b493aa944105461705b46f6
Iñaki Vladimir Moix <iniakivmoix@gmail.com>
2023-06-14 02:12:32 -0300

    Hojas de estilo
    Preparacion del componente main y cambios en las hojas de estilo

--- pokedex/src/pages/Pokemons.tsx
+++ pokedex/src/pages/Pokemons.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer';
+import { Link } from 'react-router-dom';
+
+import BulbasaurPic from "../assets/bulbasaur.gif";
+import styles from "./pokemons.module.css";
 
 const Pokemons = () => {
   const [query, setQuery] = useState("");
@@ -8,7 +12,15 @@ const Pokemons = () => {
     <>
     <Header query={query} setQuery={setQuery}/>
       <main>
-        <div>Pokemons</div>
+        <nav>
+          <Link className={styles.listItems} to="/">
+            <img className={styles.listItemsIcon} src={BulbasaurPic} alt="Bulbasaur" />
+            <div className={styles.listItemsText}>
+              <span>Bulbasaur</span>
+              <span>001</span>
+            </div>
+          </Link>
+        </nav>
       </main>
       <Footer />
     </>
