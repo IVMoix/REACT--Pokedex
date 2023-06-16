33e4f20391b5c2455d4ccd682bab5a7d88203420
Iñaki Vladimir Moix <iniakivmoix@gmail.com>
2023-06-16 01:35:57 -0300

    Incorporacion de las apis
    Incorporacion de las apis, codigo css y cambios menores en el index.html

--- pokedex/src/pages/Pokemons.tsx
+++ pokedex/src/pages/Pokemons.tsx
@@ -1,30 +1,65 @@
-import React, { useState } from 'react'
-import Header from '../components/Header'
-import Footer from '../components/Footer';
-import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { fetchPokemons } from "../api/fetchPokemons";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+import LoadingScreen from "../components/LoadingScreen";
 
-import BulbasaurPic from "../assets/bulbasaur.gif";
+import { Pokemon } from "../types/types";
+import { waitFor } from "../utils/utils"
 import styles from "./pokemons.module.css";
 
 const Pokemons = () => {
   const [query, setQuery] = useState("");
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchAllPokemons = async () => {
+      setIsLoading(true);
+      await waitFor(1000);
+      const allPokemons = await fetchPokemons();
+      setPokemons(allPokemons);
+      setIsLoading(false);
+    };
+    fetchAllPokemons();
+  }, []);
+
+  if (isLoading || pokemons === null) {
+    return <LoadingScreen />;
+  }
+
+  const filteredPokemons = pokemons?.slice(0, 151).filter((pokemon) => {
+    return pokemon.name.toLowerCase().match(query.toLowerCase());
+  });
+
   return (
     <>
-    <Header query={query} setQuery={setQuery}/>
+      <Header query={query} setQuery={setQuery} />
       <main>
-        <nav>
-          <Link className={styles.listItems} to="/">
-            <img className={styles.listItemsIcon} src={BulbasaurPic} alt="Bulbasaur" />
-            <div className={styles.listItemsText}>
-              <span>Bulbasaur</span>
-              <span>001</span>
-            </div>
-          </Link>
+        <nav className={styles.nav}>
+          {filteredPokemons?.slice(0, 151).map((pokemon) => (
+            <Link
+              key={pokemon.id}
+              className={styles.listItem}
+              to={`/pokemons/${pokemon.name.toLowerCase()}`}
+            >
+              <img
+                className={styles.listItemIcon}
+                src={pokemon.imgSrc}
+                alt={pokemon.name}
+              />
+              <div className={styles.listItemText}>
+                <span>{pokemon.name}</span>
+                <span>{pokemon.id}</span>
+              </div>
+            </Link>
+          ))}
         </nav>
       </main>
       <Footer />
     </>
-  )
-}
+  );
+};
 
-export default Pokemons
+export default Pokemons;
